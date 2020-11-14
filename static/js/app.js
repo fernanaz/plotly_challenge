b4bee304f6e542551b7bdedc08e4d51d4c301c80
fernanaz <67528464+fernanaz@users.noreply.github.com>
2020-11-14 07:21:50 -0600

    uploading Metadata sampler code

--- static/js/app.js
+++ static/js/app.js
@@ -4,3 +4,27 @@ function process() {
 
     console.log("id", targetId);
 };
+
+function Metadata(sample){
+    d3.json("samples.json").then((data)=> {
+
+        var metadata = data.metadata;
+
+        var firstValueList = metadata.filter(sampleObj => sampleObj.id == sample);
+
+        var metaResult = firstValueList[0];
+
+        var demoCard = d3.select('#sample-metadata');
+        demoCard.html("");
+
+        Object.entries(metaResult).forEach(([key, value]) => {
+            demoCard.append('h5').text(`${key}:${value}`)
+
+        });
+
+    })
+
+
+
+
+    }
