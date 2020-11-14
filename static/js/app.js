2d41d9d5e301e44d07763144c207c238f587e06d
fernanaz <67528464+fernanaz@users.noreply.github.com>
2020-11-14 07:34:04 -0600

    Update app.js
    calling metadata variables

--- static/js/app.js
+++ static/js/app.js
@@ -5,6 +5,7 @@ function process() {
     console.log("id", targetId);
 };
 
+//initializing metadata
 function Metadata(sample){
     d3.json("samples.json").then((data)=> {
 
@@ -21,10 +22,30 @@ function Metadata(sample){
             demoCard.append('h5').text(`${key}:${value}`)
 
         });
+    }
 
-    })
+    }
 
-
-
-
+    function buildCharts(idx) { 
+        d3.json("samples.json").then((data) => {
+            var samples = data.samples;
+            var resultArray = samples.filter(sampleObj => sampleObj.id == idx);
+            var result = resultArray[0];
+            console.log(result);
+            var sampleValues = result.sample_values;
+            var filteredValues = sampleValues.slice(0, 10);
+            filteredValues = filteredValues.reverse();
+            var otuIds = result.otu_ids;
+            var otuLabels = result.otu_labels;
+//metadata variables being called
+            var metadata = data.metadata;
+            var metaArray = metadata.filter(sampleObj => sampleObj.id == idx);
+            var metaResult = metaArray[0];
+            var wfreq = metaResult.wfreq;
+            console.log(sampleValues);
+            console.log(filteredValues);
+            console.log(otuIds);
+            console.log(otuLabels);
+            console.log(wfreq);
+        }
     }
