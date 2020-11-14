89bd5cbe9b789ba737936dd9e4b65d2224b7dd5f
fernanaz <67528464+fernanaz@users.noreply.github.com>
2020-11-14 07:41:28 -0600

    Update app.js
    has issues compiling

--- static/js/app.js
+++ static/js/app.js
@@ -5,7 +5,7 @@ function process() {
     console.log("id", targetId);
 };
 
-//initializing metadata
+//initializing metadata//
 function Metadata(sample){
     d3.json("samples.json").then((data)=> {
 
@@ -22,7 +22,7 @@ function Metadata(sample){
             demoCard.append('h5').text(`${key}:${value}`)
 
         });
-    }
+    })
 
     }
 
@@ -49,3 +49,109 @@ function Metadata(sample){
             console.log(wfreq);
         }
     }
+//bar chart
+let trace1 = {
+    x: filteredValues, 
+    y: [`OTU ${otuIds[0]}`, `OTU ${otuIds[1]}`, `OTU ${otuIds[2]}`, `OTU ${otuIds[3]}`, `OTU ${otuIds[4]}`,
+    `OTU ${otuIds[5]}`, `OTU ${otuIds[6]}`, `OTU ${otuIds[7]}`, `OTU ${otuIds[8]}`, `OTU ${otuIds[9]}`], //not ok
+    text: otuLabels,
+    type: "bar",
+    orientation: "h",
+};
+
+let traces = [trace1];
+
+let layout = {
+    title: "Top 10 Microbial Species"
+};
+
+Plotly.newPlot("bar", traces, layout);
+// bubble chart
+let trace2 = {
+    x: otuIds,
+    y: sampleValues,
+    text: otuLabels,
+    mode: 'markers',
+    marker: {
+        size: sampleValues,
+        color: otuIds,
+        colorscale: "Earth",
+    }
+
+}
+let bubble = [trace2];
+
+let layout2 = {
+    title: "Frequency of Microbial Species",
+    xaxis: {
+        title: "OTU Id"
+    },
+
+};
+
+Plotly.newPlot("bubble", bubble, layout2)
+
+
+console.log(wfreq);
+
+
+//gauge chart (Bonus) https://plotly.com/javascript/gauge-charts/
+var gauge = [
+    {
+        domain: { x: [0, 9], y: [0, 9] },
+        value: wfreq,
+        title: { text: "Frequency of Washing: Scrubs Per Week" },
+        type: "indicator",
+        mode: "gauge+number",
+        gauge: {
+            axis: { 'range': [0, 9] },
+            'steps': [
+                { 'range': [0, 3], 'color': '#F6B8B8' },
+                { 'range': [3, 6], 'color': "#F5F6B8" },
+                { 'range': [6, 9], 'color': "#B8F6C4" },],
+        }
+
+    },
+];
+
+var layout3 = { width: 600, height: 500, margin: { t: 0, b: 0 } };
+Plotly.newPlot('gauge', gauge, layout3);
+});
+
+}
+
+
+
+function init(){
+d3.json("samples.json").then((importedData) => {
+console.log(importedData);
+const data = importedData;
+
+var dropdownMenu = d3.select("#selDataset");
+
+let sampleId = data.names;
+console.log(sampleId);
+
+// loop for dropdown
+for (i = 0; i < sampleId.length; i++) {
+    var option = dropdownMenu.append('option');
+    option.attr('value', sampleId[i]);
+    option.text(sampleId[i]);
+};
+
+// Using the first sample from the list to build the initial dashboard
+var firstSample = sampleId[0];
+buildCharts(firstSample);
+Metadata(firstSample);
+});
+}
+
+function optionChanged(newValue){
+buildCharts(newValue);
+Metadata(newValue);
+
+}
+
+init();
+
+
